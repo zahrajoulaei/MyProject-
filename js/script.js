26752810d50ff4b85fac4af207e78d1d3aa79609
Zahra <joulaei@zentexco.com>
2015-08-27 12:09:31 +0430

    add all project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+	 var cost = $('.cost').val();
+	 var num  = $('.num').val();
+	 var total = num*cost;
+	 $(document).on('click' , 'cost' , function(){
+	 	console.log('total')
+
+
+	 	});
+		
+	
+});
